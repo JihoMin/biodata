4d41082a819c38b0018e10017248319cecc609ca
grtonksj <supksj@gmail.com>
2018-08-07 14:12:14 +0900

    커밋

---
+++ client/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Login</h1>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      microPosts: [],
+      error: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
