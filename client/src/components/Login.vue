19ac2a5147f7d22eafe9f3475b891fbcba6b4043
grtonksj <supksj@gmail.com>
2018-08-08 16:29:48 +0900

    login proto

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,5 +1,36 @@
 <template>
-<h1>Login</h1>
+  <div id="login">
+    <h1>Welcome!</h1>
+    <input type="text" name="username"  placeholder="Username"/>
+    <br />
+    <input type="password" name="password"  placeholder="Password"/>
+    <br />
+    <!-- Lost your password? <button type ="button" >Login</button> -->
+    <div class="form-block form__actions">
+                <router-link to="/password-reset">Lost your password?</router-link>
+                <button class="button button--green form__submit">Login</button>
+            </div>
+  </div>
+  <!-- <div class = "container">
+    <div class ="panel panel-primary">
+      <div class ="panel-heading">
+        <h3 class = "panel-title">Welcome</h3>
+      </div>
+      <div class="panel-body">
+        <form>
+          <div class ="form-group">
+            <label for="email" class="control-label sr-only"></label>
+            <input type="email" class="form-control" id="email"  placeholder="Email">
+          </div>
+          <div class ="form-group">
+            <label for="password" class="control-label sr-only"></label>
+            <input type="password" class="form-control" id="password"  placeholder="Password">
+          </div>
+          <button class="btn btn-primary pull-right">Sign In</button>
+        </form>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
